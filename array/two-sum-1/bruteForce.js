ed5f609b42eb782706b1e563f4c3732ec92ccd35
Sergey Popov <serge.p@speedandfunction.com>
2018-05-27 07:25:59 -0400

    1. Two Sum (Brute Force)

---
+++ array/two-sum-1/bruteForce.js
@@ -0,0 +1,25 @@
+'use strict';
+/*
+ Solution:
+
+ O(n^2) runtime, O(1) space:
+ */
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+function twoSum(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+};
+
+console.log(twoSum([2, 7, 11, 15], 17));
+
