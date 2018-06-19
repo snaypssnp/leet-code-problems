fe9da5024df612be1a4619b7e05da43695408016
Sergey Popov <serge.p@speedandfunction.com>
2018-06-19 17:53:07 +0300

    94. Binary Tree Inorder Traversal (remove example)

--- binary-tree/binary-tree-inorder-traversal-94/firstSolution.js
+++ binary-tree/binary-tree-inorder-traversal-94/firstSolution.js
@@ -1,6 +1,6 @@
 /**
  * https://leetcode.com/problems/binary-tree-inorder-traversal/description/
- * 
+ *
  * The time complexity is O(n)
  * The space complexity is O(log n)
  */
@@ -15,17 +15,6 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
-
-function TreeNode(val) {
-  this.val = val;
-  this.left = this.right = null;
-}
-
-const root = new TreeNode(1);
-
-root.right = new TreeNode(2);
-
-root.right.left = new TreeNode(3);
 
 function inorderTraversal(root) {
   const result = [];
@@ -47,6 +36,3 @@ function inOrder(node, result) {
     inOrder(node.right, result);
   }
 }
-
-
-console.log(inorderTraversal(root));
