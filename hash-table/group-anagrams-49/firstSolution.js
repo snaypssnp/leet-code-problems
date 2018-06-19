2d322bf55eea37cdf4f1ed9b8b451b4b930bfa8a
Sergey Popov <serge.p@speedandfunction.com>
2018-06-19 18:05:12 +0300

    49. Group Anagrams (update first solution)

--- hash-table/group-anagrams-49/firstSolution.js
+++ hash-table/group-anagrams-49/firstSolution.js
@@ -1,6 +1,6 @@
 /*
  https://leetcode.com/problems/group-anagrams/description/
- 
+
  n - strs length, m - max word length
  Time Complexity: O(m * n * log n)
  Space Complexity: O(m * n)
@@ -9,27 +9,20 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
-var groupAnagrams = function(strs) {
-  const result = [];
+function groupAnagrams (strs) {
   const hash = new Map();
 
   for (let i = 0, j = 0; i < strs.length; i++) {
     const word = strs[i].split('').sort().join();
 
     if (!hash.has(word)) {
-      hash.set(word, j++);
-      result.push([]);
+      hash.set(word, []);
     }
 
-    const index = hash.get(word);
-
-    result[index].push(strs[i]);
+    hash.get(word).push(strs[i]);
   }
 
-  return result;
+  return [...hash.values()];
 };
 
 console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
-
-
-
