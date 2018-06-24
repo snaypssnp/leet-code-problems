7c8a7dd7cea724e732753fe699ee457660f612dc
Sergey Popov <serge.p@speedandfunction.com>
2018-06-24 15:55:16 +0300

    155. Min Stack

---
+++ stack/min-stack-155/index.js
@@ -0,0 +1,45 @@
+class MinStack {
+  constructor() {
+    this._stack = [];
+    this._minStack = [];
+  }
+
+  push(v) {
+    this._stack.push(v);
+    const len = this._minStack.length;
+
+    if (len === 0 || this._minStack[len - 1] >= v) {
+      this._minStack.push(v);
+    }
+  }
+
+  pop() {
+    const v = this._stack.pop();
+
+    if (v === this._minStack[this._minStack.length - 1]) {
+      this._minStack.pop();
+    }
+  }
+
+  top() {
+    return this._stack[this._stack.length - 1];
+  }
+
+  getMin() {
+    return this._minStack[this._minStack.length - 1];
+  }
+}
+
+const stack = new MinStack();
+
+stack.push(-2);
+stack.push(0);
+
+stack.push(-3);
+
+stack.pop();
+
+console.log(stack.getMin());
+
+console.log(stack.top());
+
