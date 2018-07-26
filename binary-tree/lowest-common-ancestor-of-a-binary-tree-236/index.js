5a7a420fc4847c153beb64cbc82e08f8ec7f5d4b
Sergey Popov <serge.p@speedandfunction.com>
2018-07-26 13:02:21 +0300

    236. Lowest Common Ancestor of a Binary Tree

--- binary-tree/lowest-common-ancestor-of-a-binary-tree-236/index.js
+++ binary-tree/lowest-common-ancestor-of-a-binary-tree-236/index.js
@@ -1,3 +1,33 @@
 /**
- * Created by sergejpopov on 26/07/18.
+ * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/description/
+ * 
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
  */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(node, n1, n2) {
+  if (node === null) {
+    return null;
+  }
+
+  if (node === n1 || node === n2) {
+    return node;
+  }
+
+  const left = lowestCommonAncestor(root.left, n1, n2);
+  const right = lowestCommonAncestor(root.right, n1, n2);
+
+  if (left !== null && right !== null) {
+    return node;
+  }
+
+  return left || right;
+};
