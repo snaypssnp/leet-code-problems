727dd500f4e982687c21c10d88f13a0053957f81
Sergey Popov <serge.p@speedandfunction.com>
2018-08-11 09:43:36 +0300

    565. Array Nesting

---
+++ array-string/array-nesting-565/index.js
@@ -0,0 +1,28 @@
+/**
+ * https://leetcode.com/problems/array-nesting/description/
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+function arrayNesting(nums) {
+  const set = new Set();
+  let max = 0;
+
+  for (let i  = 0; i < nums.length; i++) {
+    if (set.has(i)) {
+      break;
+    }
+
+    let j = nums[i];
+    let count = 0;
+
+    do {
+      j = nums[j];
+      count++;
+    } while (j !== nums[i]);
+
+    max = Math.max(count, max)
+  }
+
+  return max;
+};
